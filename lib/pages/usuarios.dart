a0b064815aada9bc7734a9bfddcbc12bc6231e14
unknown <anthonphax@gmail.com>
2023-09-29 20:32:37 -0300

    formulario-alunos

--- lib/pages/usuarios.dart
+++ lib/pages/usuarios.dart
@@ -1,67 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'dart:math';
 
-import '../models/usuario_model.dart';
+class Usuario {
+  final int id;
+  final String nome;
+  final String telefone;
+  final String email;
+  final DateTime dataNascimento;
+  final String fotoUrl;
+
+  Usuario({
+    required this.id,
+    required this.nome,
+    required this.telefone,
+    required this.email,
+    required this.dataNascimento,
+    required this.fotoUrl,
+  });
+}
 
 class Usuarios extends StatefulWidget {
+  const Usuarios({Key? key});
+
   @override
-  State<Usuarios> createState() {
-    return UsuariosState();
-  }
+  State<Usuarios> createState() => UsuariosState();
 }
 
 class UsuariosState extends State<Usuarios> {
-  /// Lista de objetos da classe Usuario
-  List<Usuario> _lista = [];
-  
-  @override
-  void initState() {
-    super.initState();
-    _lista.add(
-      Usuario(
-        nome:"Fulano de Tal", 
-        email:"fulano@mail.com",
-        urlFoto:"https://i.pravatar.cc/300?img=5"
-      )
-    );
-    _lista.add(
-      Usuario(
-        nome: "Beltrano de Tal", 
-        urlFoto: "https://i.pravatar.cc/300?img=12"
-      )
-    );
-    _lista.add(
-      Usuario(
-        nome: "Ciclano de Tal",
-        urlFoto: "https://i.pravatar.cc/300?img=25",
-        email: "ciclano@mail.com"
-      )
-    );
-  }
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController telefoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController dataNascimentoController =
+      TextEditingController();
+
+  List<Usuario> listaUsuarios = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Usuários")),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final id = 5;
-          final urlFoto = 'https://i.pravatar.cc/300?img=' + id.toString();
-          return ListTile(
-            leading: ClipOval(
-              child: Image.network(_lista[index].urlFoto)
+      appBar: AppBar(title: const Text("Lista de Usuários")),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: nomeController,
+              decoration: const InputDecoration(
+                labelText: "Nome",
+                icon: Icon(Icons.person),
+              ),
             ),
-            title: Text(_lista[index].nome),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(_lista[index].email)
-              ]
-            )
-            // nome, telefone, e-mail 
-          );
-        }
-      )
+            TextFormField(
+              controller: telefoneController,
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(
+                labelText: "Telefone",
+                icon: Icon(Icons.phone),
+              ),
+            ),
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "E-mail",
+                icon: Icon(Icons.email),
+              ),
+            ),
+            TextFormField(
+              controller: dataNascimentoController,
+              readOnly: true,
+              decoration: const InputDecoration(
+                labelText: "Data de Nascimento",
+                icon: Icon(Icons.calendar_today),
+              ),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                );
+                if (pickedDate != null) {
+                  String dataFormatada =
+                      DateFormat('dd/MM/yyyy').format(pickedDate);
+                  setState(() {
+                    dataNascimentoController.text = dataFormatada;
+                  });
+                }
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                int id = Random().nextInt(69) + 1;
+                Usuario novoUsuario = Usuario(
+                  id: id,
+                  nome: nomeController.text,
+                  telefone: telefoneController.text,
+                  email: emailController.text,
+                  dataNascimento: DateFormat('dd/MM/yyyy')
+                      .parse(dataNascimentoController.text),
+                  fotoUrl:
+                      "https://i.pravatar.cc/300?img=" + id.toString(), 
+                );
+                setState(() {
+                  listaUsuarios.add(novoUsuario);
+                });
+
+                nomeController.clear();
+                telefoneController.clear();
+                emailController.clear();
+                dataNascimentoController.clear();
+              },
+              child: const Text("Adicionar Usuário"),
+            ),
+            const Divider(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: listaUsuarios.length,
+                itemBuilder: (context, index) {
+                  Usuario usuario = listaUsuarios[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(usuario.fotoUrl),
+                      radius: 15.0,
+                    ),
+                    title: Text(usuario.nome),
+                    subtitle: Text(usuario.email),
+                    onTap: () {},
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
